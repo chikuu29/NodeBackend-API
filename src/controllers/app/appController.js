12a41f71d17e495cdf6e9fab8d78e6a0158105ba
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-09-08 18:08:03 +0530

    Update appController.js

--- src/controllers/app/appController.js
+++ src/controllers/app/appController.js
@@ -1,45 +1,45 @@
 
 
 // const MongoDBManager = require('../../commonServices/mongoServices');
-const {mongoClient}=require('../../services/mongoService')
+const { mongoClient } = require('../../services/mongoService')
 
 
 
 // const mongoConfig = readJsonFiles('./config/mongoConfig.json');
 
-const generateConfig=async(req,res)=>{
+const generateConfig = async (req, res) => {
 
     try {
         // var refresh_token = req.cookies['refresh_token']
-        const projectName = req.body.projectName;
-        const app_config = await mongoClient.fetch("app_config", {type:"menu",id:1}, {});
-        if(app_config.length!=0){
+        // const projectName = req.body.projectName;
+        const app_config = await mongoClient.fetch("app_config", { type: "menu", id: 1 }, {});
+        if (app_config.length != 0) {
             // const message_info = { error: `User: ${userData.userName} already exists`, projectName, 'success': false, message: 'User already exists' };
             // logInfo({ ...message_info });
-            const response={
-                success:true,
-                result:{'config':app_config[0]['config']}
+            const response = {
+                success: true,
+                result: { 'config': app_config[0]['config'] }
             }
             return res.status(200).json(response);
-        }else{
+        } else {
             return res.status(200).json({
-                "success":false,
-                "result":[]
+                "success": false,
+                "result": []
             })
         }
 
-        
+
     } catch (error) {
         res.status(500).json({
             success: false,
             message: error.message,
-          });
-        
+        });
+
     }
 
 }
 
 
-module.exports={
+module.exports = {
     generateConfig
 }
