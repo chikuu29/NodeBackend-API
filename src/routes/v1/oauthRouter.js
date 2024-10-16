4e6fe17848a3d77878d875508367ab5f497583f8
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-10-16 19:43:15 +0530

    done

--- src/routes/v1/oauthRouter.js
+++ src/routes/v1/oauthRouter.js
@@ -6,12 +6,16 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const { googleLogin } = require('../../controllers/v1/auth/userController');
 // Google OAuth Strategy setup
+const callbackURL = process.env.NODE_ENV === 'production'
+    ? 'https://myomspanel.onrender.com/api/v1/oauth/google/callback'  // Production URL
+    : '/v1/oauth/google/callback';  // Development URL
+
 passport.use(
     new GoogleStrategy(
         {
             clientID: configLoader.get('serverConfig')['OAUTH_LOGIN_SYSTEM']['GOOGLE_AUTH_CREDENTIALS']['client_id'],
             clientSecret: configLoader.get('serverConfig')['OAUTH_LOGIN_SYSTEM']['GOOGLE_AUTH_CREDENTIALS']['client_secret'],
-            callbackURL: '/v1/oauth/google/callback',
+            callbackURL: process.env.GOOGLE_OAUTH_CALLBACK_URL || callbackURL,
         },
         (accessToken, refreshToken, profile, done) => {
             // You can customize how user information from Google is handled here
