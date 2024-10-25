018f9586956123a4604206873a5b809643c7d029
SURYANARAYAN BISWAL <cchiku1999@gmail.com>
2024-10-25 23:03:20 +0530

    Done

--- src/routes/v1/authRouter.js
+++ src/routes/v1/authRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const { checkSession } = require("../../middlewares/identifyApplicationMiddlewares")
-const { loginUser, createSession, logoutUser } = require('../../controllers/v1/auth/userController');
+const { loginUser, createSession, logoutUser, registerUser } = require('../../controllers/v1/auth/userController');
 
 
-router.post("/register",)
+router.post("/register",registerUser)
 router.post('/login', loginUser);
 router.get('/logout', checkSession, logoutUser);
 router.get("/me", checkSession, createSession);
